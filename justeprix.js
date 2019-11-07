8dd60c8b3a93baa46d89df7c3a23ee270efc31f8
vincent vissers <huskyix@MacBook-Pro-de-vincent.local>
2019-11-07 23:21:40 +0100

    first commit

---
+++ justeprix.js
@@ -0,0 +1,39 @@
+let prix;
+//generation du chiffre aleatoire
+prix = Math.round(Math.random(1,100)*(100-1)+1);
+console.log(prix);
+//lecteur input
+
+let comfirme,nbr,choose;
+document.getElementById('run').addEventListener('click',() =>{
+    nbr = document.getElementById('target').value;
+    choose = parseInt(nbr)
+    console.log(choose)
+    jeux()
+    function jeux(){
+        if (/\D/.test(choose)){
+            document.getElementById("indice").innerHTML= "Introduit un chiffres entre 1 & 100"
+            jeux()
+        }
+        if (choose == prix) {
+            document.getElementById("indice").innerHTML="Félicitation tu as trouvé le juste prix !!!"
+            comfirme = confirm("veux tu rejouer ?")
+        }
+        else if (choose<prix) {
+            document.getElementById("indice").innerHTML="tu es en dessous"
+            jeux()
+        } 
+        else {
+            document.getElementById("indice").innerHTML="tu es au dessus"
+            jeux()
+        }
+        if (comfirme == true) {
+            prix = Math.round(Math.random(1,100)*(100-1)+1);
+            console.log(prix)
+            jeux()
+        }
+        else{
+            document.getElementById("indice").innerHTML="Merci d'avoir participer a ce petit jeu !"
+        }
+    }
+})
